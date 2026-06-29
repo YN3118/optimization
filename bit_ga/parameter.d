897dc316ab8c062b7680bc1e78224f477b3837e5
yuugo <yuugo_baseball@icloud.com>
2026-06-29 12:32:22 +0900

    fix

--- bit_ga/parameter.d
+++
@@ -1,4 +0,0 @@
-src/parameter.o: src/parameter.cpp include/parameter.hpp \
- include/config.hpp
-include/parameter.hpp:
-include/config.hpp:
--- bit_ga/population.d
+++
@@ -1,9 +0,0 @@
-src/population.o: src/population.cpp include/population.hpp \
- include/config.hpp include/individual.hpp include/parameter.hpp \
- include/random.hpp include/evaluator.hpp
-include/population.hpp:
-include/config.hpp:
-include/individual.hpp:
-include/parameter.hpp:
-include/random.hpp:
-include/evaluator.hpp:
